f966206cb143c534055ffd9725e453d572416412
wangshuai <wangshuai@ata.net.cn>
2013-08-08 17:10:56 +0800

    statics

---
+++ phottix/static/js/controllers.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('xuancaiApp.controllers', []).
+  controller('MyCtrl1', [function() {
+
+  }])
+  .controller('MyCtrl2', [function() {
+
+  }]);
